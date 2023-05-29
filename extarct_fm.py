647517b77a8d9195287f78152ee39a137d958504
Kuang Wenwei <kuangww@slurm-q01-inst-1.gpufarm.cs.hku.hk>
2023-05-30 07:58:06 +0800

    For PanNuke

--- extarct_fm.py
+++ extarct_fm.py
@@ -7,7 +7,7 @@ import numpy as np
 import torch 
 
 ROOT_PATH = './datasets/'
-DATANAME = "bowl_2018"
+DATANAME = "PanNuku"
 
 
 # def show_mask(mask, ax, random_color=False):
@@ -21,7 +21,13 @@ DATANAME = "bowl_2018"
 
 if __name__ == '__main__':
     TRAIN_PATH = os.path.join(ROOT_PATH,DATANAME)
-    train_ids = next(os.walk(TRAIN_PATH))[1]
+    OUTPUT_DIR = os.path.join(TRAIN_PATH,"features")
+
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    image_dir = os.path.join(TRAIN_PATH,"images")
+    image_names = os.listdir(image_dir)
 
     sam_checkpoint = "/userhome/cs2/kuangww/segment-anything/notebooks/models/sam_vit_h_4b8939.pth"
     model_type = "vit_h"
@@ -33,12 +39,9 @@ if __name__ == '__main__':
 
     predictor = SamPredictor(sam) 
 
-    for file_name in tqdm(train_ids):
+    for file_name in tqdm(image_names):
         path = os.path.join(TRAIN_PATH,file_name)
-        img = cv2.imread(path + '/images/' + file_name + '.png')
-        output_dir = os.path.join(path,'features')
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+        img = cv2.imread(os.path.join(image_dir,file_name))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #BGR2RGB
 
         predictor.set_image(img)
@@ -53,7 +56,7 @@ if __name__ == '__main__':
 
         # masks, scores, logits = predictor.predict(multimask_output=False)
         # import pdb;pdb.set_trace()
-        torch.save(data,os.path.join(output_dir,file_name+".pt"))
+        torch.save(data,os.path.join(OUTPUT_DIR,file_name.replace(".png",".pt")))
 
         predictor.reset_image()
 
